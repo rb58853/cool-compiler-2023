09cb8bdec0ab8a891ad58069d9034e2722ea0a02
V1s1t@nt <victorsoslatin@gmail.com>
2023-12-01 17:06:33 -0600

    update: auto test

---
+++ src/tester.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+def find_test_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.cl'):
+                yield os.path.join(root, file)
+
+def run_test(file_path):
+    process = subprocess.run(['./src/coolc.sh', file_path], capture_output=True, text=True)
+    return process.returncode, process.stdout, process.stderr
+
+
+def main():
+    test_files_directory = 'tests'  # Ajusta según la estructura de tu proyecto
+    for test_file in find_test_files(test_files_directory):
+        returncode, stdout, stderr = run_test(test_file)
+
+
+if __name__ == "__main__":
+    main()
